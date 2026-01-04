6b89b11c9d222a7a145bc59c3b0d63921f863e05
anastasiiaboiko651 <nastusaboiko651@gmail.com>
2026-01-04 18:51:01 +0200

    ..

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	var input string
+	var length int
+
+	// ---- введення довжини ----
+	for {
+		fmt.Print("Введіть довжину пароля (4..128): ")
+		fmt.Scan(&input)
+
+		n, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Помилка: введіть число.")
+			continue
+		}
+		if n < 4 || n > 128 {
+			fmt.Println("Помилка: довжина має бути від 4 до 128.")
+			continue
+		}
+
+		length = n
+		break
+	}
+
+	// ---- вибір категорій ----
+	var pool string
+
+	for {
+		pool = ""
+
+		fmt.Print("Малі літери? (1/0): ")
+		fmt.Scan(&input)
+		if input == "1" {
+			pool += "abcdefghijklmnopqrstuvwxyz"
+		}
+
+		fmt.Print("Великі літери? (1/0): ")
+		fmt.Scan(&input)
+		if input == "1" {
+			pool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		}
+
+		fmt.Print("Цифри? (1/0): ")
+		fmt.Scan(&input)
+		if input == "1" {
+			pool += "0123456789"
+		}
+
+		fmt.Print("Спецсимволи? (1/0): ")
+		fmt.Scan(&input)
+		if input == "1" {
+			pool += "!@#$%"
+		}
+
+		if pool == "" {
+			fmt.Println("Помилка: оберіть хоча б одну категорію.")
+			continue
+		}
+		break
+	}
+
+	// ---- генерація пароля ----
+	for i := 0; i < length; i++ {
+		fmt.Print(string(pool[rand.Intn(len(pool))]))
+	}
+	fmt.Println()
+}
